a96973ef143553ec69dc929b6dcc4026805f5f0e
Gagan Sapkota <here.gagan@gmail.com>
2021-04-15 13:43:44 -0600

    Additional exercise with .each method

--- codeacademy/test1.rb
+++ codeacademy/test1.rb
@@ -1,9 +1,25 @@
-puts "Welcome, dear! What's your name?"
+# puts "Welcome, dear! What's your name?"
 
-user_input = gets.chomp
+# user_input = gets.chomp
 
-user_input.downcase!
+# user_input.downcase!
 
-if user_input.include? "s"
-    print "Voila! Your name contains letter s"
+# if user_input.include? "s"
+#     print "Voila! Your name contains letter s"
+# end
+
+puts "Enter some text: "
+text = gets.chomp
+
+puts "Enter words to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+words.each do |word| 
+    if word != redact
+        print word + " "
+    else
+        print "REDACTED" + " "
+    end
 end
+  
