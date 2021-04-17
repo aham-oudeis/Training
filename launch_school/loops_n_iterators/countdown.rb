915723f075e949589ee2ae4f7d8b2dbb5ca4fc61
Gagan Sapkota <here.gagan@gmail.com>
2021-04-17 13:47:27 -0600

    countdown using recursion

--- launch_school/loops_n_iterators/countdown.rb
+++ launch_school/loops_n_iterators/countdown.rb
@@ -4,9 +4,22 @@ puts "Where do you want to start the countdown?"
 
 num = gets.chomp.to_i
 
-for i in (1..num).to_a.reverse
-  if i == 4       # skip number 4 from the countdown
-    next
+# for i in (1..num).to_a.reverse
+#   if i == 4       # skip number 4 from the countdown
+#     next
+#   end
+#   puts i
+# end
+
+# using recursion
+
+def countdown(num)
+  if num == 1
+    p num
+  else 
+    p num 
+    countdown(num-1)
   end
-  puts i
 end
+
+countdown(num)
