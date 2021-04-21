fa0c4d9b7d6928c50f0bc794767edffb7b903d27
Gagan Sapkota <here.gagan@gmail.com>
2021-04-20 18:28:41 -0600

    concataneting methods

---
+++ launch_school/little_tests/methods/greet.rb
@@ -0,0 +1,23 @@
+# def print_me
+#   puts "I am printing within the method"
+# end
+
+# def print_me
+#   "I am printing the return value!"
+# end
+
+# puts print_me
+
+def hello
+  "Hello"
+end
+
+def world
+  "World"
+end
+
+def  greet 
+  hello + " " + world
+end
+
+puts greet
