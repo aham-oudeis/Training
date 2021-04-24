cab5349596503423c8dec40bf46a91fa1bf4ae17
Gagan Sapkota <here.gagan@gmail.com>
2021-04-24 16:15:06 -0600

    adding more complexity by validating inputs

--- launch_school/Programming_Foundations/lesson_two/calculator.rb
+++ launch_school/Programming_Foundations/lesson_two/calculator.rb
@@ -1,44 +1,87 @@
-
-Kernel.puts(" Welcome to the calculator!\n")
+def prompt(str)
+  Kernel.puts("==> #{str}")
+end
 
 def operation_valid()
-  Kernel.puts("What operation would you like to perform?\nadd: a, substract: s, multiply: m, division: d")
+  command_prompt = <<-MLM
+What operation would you like to perform?
+    add:        press a 
+    substract:  press s 
+    multiply:   press m 
+    division:   press d
+  MLM
+  prompt(command_prompt)
 
   operation = Kernel.gets().chomp()
   return operation if %w(a s m d).include?(operation)
-  Kernel.puts("Please input a valid operation!")
+  prompt('Please input a valid operation! Choose a, s, m, or d.')
   operation_valid()
 end
 
-def valid_num()
+def valid_num(oper = 'not d')
   num = Kernel.gets().chomp()
-  if num.to_i.to_s == num || num.to_f.to_s == num
-    # since we are working with floats, multiplications and devision can introduce trailing new numbers. This can go wrong badly if we are looping over the results and cumulating the errors.
-    return num.to_f
+  valid_num_test = (num.to_i.to_s == num) || (num.to_f.to_s == num)
+
+  if valid_num_test
+    if oper == 'd' && num.to_i == 0
+      prompt("Division by O is not allowed!")
+      prompt('Please enter a valid number!')
+      return valid_num(oper)
+    end
+
+    return num.to_f # be wary of cumulating errors through float conversion
+
   end
-  Kernel.puts("Please enter a valid number!")
-  valid_num()
+
+  prompt('Please enter a valid number!')
+  valid_num(oper)
 end
 
-operation = operation_valid()
+prompt("What is your name?")
+name = gets.chomp
+prompt("Hi #{name}! Welcome to the calculator!")
 
-Kernel.puts("Enter the first number!")
-num_first = valid_num()
+loop do
+  operation = operation_valid()
 
-Kernel.puts("Enter the second number!")
-num_second = valid_num()
+  prompt('Enter the first number!')
+  num_first = valid_num()
 
-if operation == 'a'
-  result = num_first + num_second
-elsif operation == 's'
-  result = num_first - num_second
-elsif operation == 'm'
-  result = num_first * num_second
-else
-  result = num_first / num_second
+  prompt('Enter the second number!')
+  num_second = valid_num('d')
+
+  result = case operation
+           when 'a'
+             num_first + num_second
+           when 's'
+             num_first - num_second
+           when 'm'
+             num_first * num_second
+           else
+             num_first / num_second
+           end
+
+  doing_what = case operation
+               when 'a'
+                 'Adding'
+               when 's'
+                 'Substracting'
+               when 'm'
+                 'Multiplying'
+               else
+                 'Dividing'
+               end
+
+  prompt("#{doing_what} the two numbers...")
+  sleep(1)
+  ## to remove the errors introduced by float point,
+  ## we can just rounnd the number to 4 decimal places
+  prompt("The result is #{result.round(4)}")
+  prompt("Do you want to calculate again?
+    Press Y to calculate again.")
+  response = Kernel.gets().chomp()
+  if response.downcase().start_with?('y') == false
+    prompt("Thank you for using the calculator. Goodbye!")
+    break
+  end
 end
-
-## to remove the errors introduced by float point, 
-## we can just rounnd the number to 4 decimal places
-Kernel.puts("The result is #{result.round(4)}")
-
