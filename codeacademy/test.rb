299403bf5f304935991fb4b9f631ecd4595729b2
Gagan Sapkota <here.gagan@gmail.com>
2021-04-24 16:15:57 -0600

    not much

--- codeacademy/test.rb
+++ codeacademy/test.rb
@@ -8,12 +8,12 @@ b = Integer(gets.chomp)
 c = a % b
 
 if c == 0
-    print "#{a} is strictly divisible by #{b}"
+  print "#{a} is strictly divisible by #{b}"
 
 elsif c == 1
-    print "#{a} is just a bit shy of being perfectly divisible by #{b}. #{a} is divided by #{b} leaves #{c} as a remainder"
+  print "#{a} is just a bit shy of being perfectly divisible by #{b}. #{a} is divided by #{b} leaves #{c} as a remainder"
 
 else
-    print "#{a} is divided by #{b} leaves #{c} as a remainder"
+  print "#{a} is divided by #{b} leaves #{c} as a remainder"
 
 end
