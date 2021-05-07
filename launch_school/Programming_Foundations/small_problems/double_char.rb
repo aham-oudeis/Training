4887c15b24cabebfa0b848564a14f148ce91a31e
Gagan Sapkota <here.gagan@gmail.com>
2021-05-07 11:35:03 -0600

    doubling constants

---
+++ launch_school/Programming_Foundations/small_problems/double_char.rb
@@ -0,0 +1,30 @@
+# def repeater(str)
+#   new_str = ''
+#   str.chars.each {|char| new_str << char << char}
+#   new_str
+# end
+
+# p repeater('Hello') == "HHeelllloo"
+# p repeater("Good job!") == "GGoooodd  jjoobb!!"
+# p repeater('') == ''
+
+ALPHABETS = ('A'..'Z').to_a
+VOWELS = %W(A E I O U)
+CONSONANTS = ALPHABETS - VOWELS
+
+def double_consonants(str)
+  new_str = ''
+  str.chars.each do |char|
+    if CONSONANTS.include?(char.upcase)
+      new_str << char << char
+    else
+      new_str << char
+    end
+  end
+  new_str
+end
+
+p double_consonants('String') == "SSttrrinngg"
+p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
+p double_consonants("July 4th") == "JJullyy 4tthh"
+p double_consonants('') == ""
