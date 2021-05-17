61b06574e8a486f051a018813f3c91584fb9b201
Gagan Sapkota <here.gagan@gmail.com>
2021-05-17 11:40:45 -0600

    correcting and refactoring the old code

--- launch_school/Programming_Foundations/lesson_five/p15.rb
+++ launch_school/Programming_Foundations/lesson_five/p15.rb
@@ -5,35 +5,28 @@ Given this data structure write some code to return an array which contains only
 
 arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
 
-# the central problem with select is that it only sees two elements in the 
-# array and does not go deeper in the second hash
-# if we only wanted to select among the two, this would be fine
+=begin
+the central problem with select is that it only sees two elements in
+the array and does not go deeper in the second hash
+if we only wanted to select among the two, this would be fine
+=end
 
 # p (arr.select do |hash|
-#   arr_w_values = hash.values
-#   if arr_w_values.size < 1
+#     arr_w_values = hash.values.flatten
 #     arr_w_values.all?(&:even?)
-#   else
-#     arr_w_values.map {|nest_arr| nest_arr.all?(&:even?)}.all?
-#   end
 # end)
 
-# but it seems like the problem is about returning something like this 
+# a bit more concise
+p arr.select {|hash| hash.values.flatten.all?(&:even?)}
+
+# but a more interesting versioon of the problem
+# is about returning something like this
 # [{b: [2, 4, 6]}, {d: [4]}, {e: [8]}, {f: [6, 10]}]
 
 # p (arr.each_with_object([]) do |hash, array|
-#       array << (hash.each_with_object({}) do |(k, v), inner_hash|
-#                   inner_hash[k] = v if v.all?(&:even?)
-#                 end)
-#     end)
-
-# that does something but not what I want; it creates an empty hash for 
-# each hash pair even if does not have even numbers in its values
-
-p (arr.each_with_object([]) do |hash, array|
-    hash.each do |k, v|
-      inner_hash = {k => v}
-      # inner_hash[k] = v
-      array << inner_hash if v.all?(&:even?)
-    end
-end)
+#     hash.each do |k, v|
+#       inner_hash = {k => v}
+#       # or, inner_hash[k] = v
+#       array << inner_hash if v.all?(&:even?)
+#     end
+# end)
