8dd603923a1a2c73dd54f10bbade79fdb08bbd89
Gagan Sapkota <here.gagan@gmail.com>
2021-07-30 19:38:34 -0600

    working through Debugging exercises

---
+++ launch_school/RB120-OOP/Exercises/Debugging/animal_kingdom.rb
@@ -0,0 +1,59 @@
+class Animal
+  def initialize(diet, superpower)
+    @diet = diet
+    @superpower = superpower
+  end
+
+  def move
+    puts "I'm moving!"
+  end
+
+  def superpower
+    puts "I can #{@superpower}!"
+  end
+end
+
+class Fish < Animal
+  def move
+    puts "I'm swimming!"
+  end
+end
+
+class Bird < Animal
+end
+
+class FlightlessBird < Bird
+  def initialize(diet, superpower)
+    super
+  end
+
+  def move
+    puts "I'm running!"
+  end
+end
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    super(diet, superpower)
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# Examples
+
+unicornfish = Fish.new(:herbivore, 'breathe underwater')
+penguin = FlightlessBird.new(:carnivore, 'drink sea water')
+robin = SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
+
+# the problem was with calling the super method with all three arguments
+# passed in to the initialize method. When super is called without bracets,
+# all the arguments passed to the initialize method gets passed to the
+# super method. But this gives us an error because the initialize method
+# in the parent class only takes two arguments, not three.
+
+# the FlightlessBird initialize method is not necessary because it
+# gets called by default.
---
+++ launch_school/RB120-OOP/Exercises/Debugging/community_library.rb
@@ -0,0 +1,44 @@
+class Library
+  attr_accessor :address, :phone, :books
+
+  def initialize(address, phone)
+    @address = address
+    @phone = phone
+    @books = []
+  end
+
+  def check_in(book)
+    books.push(book)
+  end
+end
+
+class Book
+  attr_accessor :title, :author, :isbn
+
+  def initialize(title, author, isbn)
+    @title = title
+    @author = author
+    @isbn = isbn
+  end
+
+  def display_data
+    puts "---------------"
+    puts "Title: #{title}"
+    puts "Author: #{author}"
+    puts "ISBN: #{isbn}"
+    puts "---------------"
+  end
+end
+
+community_library = Library.new('123 Main St.', '555-232-5652')
+learn_to_program = Book.new('Learn to Program', 'Chris Pine', '978-1934356364')
+little_women = Book.new('Little Women', 'Louisa May Alcott', '978-1420951080')
+wrinkle_in_time = Book.new('A Wrinkle in Time', 'Madeleine L\'Engle', '978-0312367541')
+
+community_library.check_in(learn_to_program)
+community_library.check_in(little_women)
+community_library.check_in(wrinkle_in_time)
+
+community_library.books.map(&:display_data)
+
+# the main issue is that we were calling the display_data method on an array object, but we have defined the method only for the Book object. So, we need to call the method display_data on the Book objects that populate the array returned by community_library.books
