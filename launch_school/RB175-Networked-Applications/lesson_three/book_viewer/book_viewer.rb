60a47334df381fb8550829686b41e2d747040ce8
Gagan Sapkota <here.gagan@gmail.com>
2021-11-02 12:50:39 -0700

    advanced search features

--- launch_school/RB175-Networked-Applications/lesson_three/book_viewer/book_viewer.rb
+++ launch_school/RB175-Networked-Applications/lesson_three/book_viewer/book_viewer.rb
@@ -2,26 +2,76 @@ require "sinatra"
 require "sinatra/reloader"
 require 'tilt/erubis'
 
+before do 
+  @list_of_contents = File.readlines "data/toc.txt"
+end
+
+helpers do 
+  def slugify(text)
+    text.downcase.gsub(/\s+/, "-").gsub(/[^\w-]/, "")
+  end
+
+  def in_paragraphs(text)
+    text.split("\n\n")
+  end
+
+  def each_chapter
+    @list_of_contents.each_with_index do |chapter, idx|
+      number = idx + 1
+      contents = File.read "data/chp#{number}.txt"
+
+      yield chapter, number, contents
+    end
+  end
+
+  def matching_chapters(query)
+    results = []
+
+    return results if !query || query.empty?
+
+    each_chapter do |chapter, number, contents|
+      if contents.include? query
+        contents_list = contents.split("\n\n")
+        results << {number: number, chapter: chapter, contents_list: contents_list}
+      end
+    end
+
+    results
+  end
+
+  def bold_matching(para, match)
+    para.gsub(match, "<strong>#{match}</strong>")
+  end
+end
+
+not_found do 
+  redirect "/"
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Homes"
-  @list_of_contents = File.readlines "data/toc.txt"
+
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @list_of_contents = File.readlines "data/toc.txt"
-  @heading = @list_of_contents[0]
-  @paragraphs = File.readlines "data/chp1.txt"
-  erb :chapter
+get "/chapters/:name" do  
+  chapter_index = params['name'].to_i - 1
+  chapter_index = 0 unless (0..@list_of_contents.size).include?(chapter_index)
+
+  chapter_name = @list_of_contents[chapter_index]
+  @title = "Chapter #{chapter_index + 1}: #{chapter_name}"
+  @heading = chapter_name
+
+  @chapter = File.read "data/chp#{chapter_index + 1}.txt"
+  
+  erb :chapter 
 end
 
-get "/chapters/:name" do
-  @title = "Chapter #{params['name']}"
-  @list_of_contents = File.readlines "data/toc.txt"
-  chapter_index = params['name'].to_i - 1
-  chapter_index = 0 if chapter_index >= @list_of_contents.size
-  @heading = @list_of_contents[chapter_index]
-  @paragraphs = File.readlines "data/chp#{chapter_index + 1}.txt"
-  erb :chapter 
+get "/search" do
+  @title = "search"
+  @heading = "Search"
+
+  @search_result = matching_chapters(params[:query])
+
+  erb :search
 end
