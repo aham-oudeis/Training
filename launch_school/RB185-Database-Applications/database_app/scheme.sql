48ee0bdad5976530d158cf2a069279873d5076d3
Gagan Sapkota <here.gagan@gmail.com>
2022-01-15 21:09:53 -0700

    first pass-upto sql injection

---
+++ launch_school/RB185-Database-Applications/database_app/scheme.sql
@@ -0,0 +1,12 @@
+CREATE TABLE expenses (
+  id serial PRIMARY KEY,
+  amount numeric(6, 2) NOT NULL CHECK(amount > 0),
+  memo text NOT NULL,
+  created_on timestamp NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO expenses
+            (amount, memo, created_on)
+     VALUES (14.56, 'Pencils', NOW()),
+            (3.29, 'Coffee', NOW()),
+            (49.99, 'Text Editor', NOW());
